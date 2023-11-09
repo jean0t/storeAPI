ec6f05eef59acedaa6dbc2ca9b47c5e0f8707fff
jean0t <joaomouryf@hotmail.com>
2023-11-08 22:23:42 -0300

    first route created

--- app.py
+++ app.py
@@ -2,5 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+@app.post()
+def create_store():
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
